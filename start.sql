fe7704364e565306b702c190c856addc30f9633d
REALmyenemy <eldani765@hotmail.com>
2019-12-03 08:10:22 +0100

    Cambios

--- start.sql
+++ start.sql
@@ -7,10 +7,99 @@ drop table if exists empresa;
 drop table if exists opinion;
 drop table if exists valoracion;
 drop table if exists usuarios;
+/*
+create table usuarios (
+	id varchar(35) primary key,
+	nombre varchar(100),
+	apellidos varchar(120),
+	pass mediumblob,
+	salt mediumblob,
+	cuentaactiva tinyint(1),
+	rol varchar(8),
+	fechanacimiento date
+);*/
+create or replace table usuarios
+(
+	id varchar(35) primary key, 
+	nombre varchar(100), 
+	apellidos varchar(120), 
+	pass varchar(200), 
+	cuentaActiva boolean, 
+	rol varchar(8), 
+	fechanacimiento date, 
+	constraint ck_rol check 
+	(rol in ('Gestor','Profesor','Alumno'))
+);
+´/*
+create table empresa (
+	id integer primary key auto_increment,
+	nombre varchar(50),
+	representante varchar(200),
+	sector varchar(30),
+	resumen varchar(255),
+	ubicacion varchar(255),
+	imagenprincipal integer,
+	aprobada tinyint(1),
+	constraint fk_emp_ima foreign key (imageprincipal) references imagenes
+);.*/
 
-create or replace table usuarios (id varchar(35) primary key, nombre varchar(100), apellidos varchar(120), pass varchar(200), cuentaActiva boolean, rol varchar(8), fechanacimiento date, constraint ck_rol check (rol in ('Gestor','Profesor','Alumno')));
+create or replace table empresa (id integer primary key auto_increment,
+ nombre varchar(50),
+ representante varchar (200),
+ sector varchar (30),
+ resumen varchar(255),
+ ubicacion varchar(255),
+ imagenPrincipal integer
+);
 
-create or replace table empresa (id integer primary key auto_increment, nombre varchar(50), representante varchar (200), sector varchar (30), resumen varchar(255), ubicacion varchar(255), imagenPrincipal integer);
+create table valoracion (
+	usuario varchar(35),
+	empresa integer,
+	valoracion integer,
+	primary key (usuario,empresa)
+);
+
+create table opinion (
+	usuario varchar(35),
+	empresa integer,
+	comentario varchar(255),
+	certificado tinyint(1),
+	opinionprofesor varchar(128),
+	constraint pk_opinion primary key (usuario,empresa),
+	constraint fk_opi_usu foreign key (usuario) references usuarios,
+	constraint fk_opi_emp foreign key (empresa) references empresa
+);
+
+create table imagenes (
+	indice integer primary key auto_increment,
+	empresa integer not null,
+	rutaimagen varchar(255)
+	constraint fk_ima_emp foreign key (empresa) references empresa
+);
+
+create table puesto (
+	empresa integer ,
+	id integer,
+	nombre varchar(10),
+	descripcion varchar(255),
+	constraint pk_puesto primary key (empresa,id),
+	constraint fk_pue_emp foreign key (empresa) references empresa
+);
+
+create table requisito (
+	empresa integer,
+	puesto integer,
+	id integer not null,
+	descripcion varchar(255),
+	constraint pk_requisito primary key (empresa,puesto,id),
+	constraint fk_requisito foreign key (empresa,puesto) references puesto
+);
+
+
+commit;
+
+
+
  
 create or replace table valoracion (usuario varchar(35) references usuarios (id), empresa integer references empresa (numEmpresa), valoracion integer, constraint pk_valoracion primary key (usuario, empresa));
 create or replace table opinion (usuario varchar(35) references usuarios (id), empresa integer references empresa (id), comentario varchar (255), certificado boolean, opinionProfesor varchar(128), constraint pk_opinion  primary key (usuario, empresa));
